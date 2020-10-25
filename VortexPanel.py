8cf6ebd4131c7f5f1a2aff03bc47bb0141163da4
mcleantom <64887735+mcleantom@users.noreply.github.com>
2020-10-25 08:59:28 +0000

    Added panels class
    Added the class panels which is used to hold all of the panels in the simulation and can calculate the constant vortex dipole strengths along each panel

--- VortexPanel.py
+++ VortexPanel.py
@@ -17,7 +17,7 @@ from mpl_toolkits.mplot3d.art3d import Poly3DCollection
 
 class Panel(object):
     """
-    Initialise a quadrilateral panel between two points in 3D space
+    Initialise a quadrilateral panel between four points in 3D space
     
     Inputs:
         x0,y0,z0 -- The coordinates of point 0
@@ -30,23 +30,65 @@ class Panel(object):
         A panel object.
     """
     
-    def __init__(self, x0, x1, x2, x3, y0, y1, y2, y3, z0, z1, z2, z3):
-        self.x = (x0, x1, x2, x3)
-        self.y = (y0, y1, y2, y3)
-        self.z = (z0, z1, z2, z3)
+    def __init__(self, x1, x2, x3, x4, y1, y2, y3, y4, z1, z2, z3, z4):
+        """
+        """
+        self.x = (x1, x2, x3, x4) # Quadrilateral x coordinates
+        self.y = (y1, y2, y3, y4) # Quadrilateral y coordinates
+        self.z = (z1, z2, z3, z4) # Quadrilateral z coordinates
         
-        self.cx = sum(self.x)/4
-        self.cy = sum(self.y)/4
-        self.cz = sum(self.z)/4
+        self.cx = sum(self.x)/4 # Panel centre x coordinate
+        self.cy = sum(self.y)/4 # Panel centre y coordinate
+        self.cz = sum(self.z)/4 # Panel centre z coordinate
         
-        self.A = np.array([(x2-x0), (y2-y0), (z2-z0)]).T
-        self.B = np.array([(x3-x1), (y3-y1), (z3-z1)]).T
-        self.S = np.abs(np.cross(self.A, self.B))/2
+        self.A = np.array([(x3-x1), (y3-y1), (z3-z1)]).T # Panel diagonal
+        self.B = np.array([(x4-x2), (y4-y2), (z4-z2)]).T # Panel diagonal
+        self.S = np.abs(np.cross(self.A, self.B))/2 # Perpendicular vector
+        
+        # Longitudinal unit vector
+        ux = (x1+x2-x3-x4)/2
+        uy = (y1+y2-y3-y4)/2
+        uz = (z1+z2-z3-z4)/2
+        
+        # Transverse unit vector
+        px = (x2+x3-x4-x1)/2
+        py = (y2+y3-y4-y1)/2
+        pz = (z2+z3-z4-z1)/2
+        
+        self.gamma = 0 # Panel vortex strength
     
     def plot(self):
+        """
+        Plot the panel in 3D space
+        """
+
         fig = plt.figure()
         ax = fig.gca(projection='3d')
         verts = [list(zip(self.x, self.y, self.z))]
         ax.add_collection3d(Poly3DCollection(verts))
         plt.show()
+
+
+class panels(object):
+    """
+    """
+    
+    def __init__(self, panels):
+        self.panels = panels
+    
+    def solve_gamma(self):
+        """
+        """
+        A, b = self._construct_A_b()
         
+    def _construct_A_b(self):
+        """
+        """
+        
+    def get_array(self):
+        """
+        """
+
+
+one_panel = Panel(0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0)
+pans = panels([one_panel])
